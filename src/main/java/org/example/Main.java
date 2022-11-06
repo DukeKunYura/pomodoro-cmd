20b232aa980f1bc2a9e7e39f6d49f4c1fe6fcdf7
DukeKunYura <duke_d_yura@rambler.ru>
2022-11-07 00:45:01 +0300

    33

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello pomodoro!");
         System.out.println("start new branch");
+        System.out.println("3");
     }
 }
